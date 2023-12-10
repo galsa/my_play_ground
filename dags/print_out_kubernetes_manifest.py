4c5331f39ec168580cb3c0321f826ce5d7fad904
galsoetendorp <gal.soetendorp@personetics.com>
2023-12-10 12:19:32 +0200

    trying some more options

--- dags/print_out_kubernetes_manifest.py
+++ dags/print_out_kubernetes_manifest.py
@@ -17,14 +17,3 @@ with DAG(
         task_id="dry_run_demo",
         do_xcom_push=True,
     )
-
-    k2 = KubernetesPodOperator(
-        task_id="test_error_message",
-        image="alpine",
-        cmds=["/bin/sh"],
-        arguments=["-c", "echo hello world; echo Custom error > /dev/termination-log; exit 1;"],
-        name="test-error-message",
-        email="airflow@example.com",
-        email_on_failure=True,
-    )
-
