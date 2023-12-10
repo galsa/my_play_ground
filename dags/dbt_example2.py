9c6b84d9e95e6fc545de60045e3c8555a4278153
galsoetendorp <gal.soetendorp@personetics.com>
2023-12-10 10:48:00 +0200

    modified dbt_kubernetes_pod_operator_example DAG

--- dags/dbt_example2.py
+++ dags/dbt_example2.py
@@ -30,18 +30,29 @@ else:
 dag = DAG('dbt_kubernetes_pod_operator_example', default_args=default_args, schedule_interval=None)
 
 # Task
-dbt_test = KubernetesPodOperator(
-    task_id="dbt-test",
-    name="dbt-test",
-    namespace=namespace,
-    image="my-dbt-image:1.0",
-    cmds=["dbt"],
-    arguments=["run", "--profiles-dir", "profiles"],
-    get_logs=True,
-    dag=dag,
-    is_delete_operator_pod=False,
-    config_file=config_file,
-    in_cluster=in_cluster,
-)
+# dbt_test = KubernetesPodOperator(
+#     task_id="dbt-test",
+#     name="dbt-test",
+#     namespace=namespace,
+#     image="my-dbt-image:1.0",
+#     cmds=["dbt"],
+#     arguments=["run", "--profiles-dir", "profiles"],
+#     get_logs=True,
+#     dag=dag,
+#     is_delete_operator_pod=False,
+#     config_file=config_file,
+#     in_cluster=in_cluster,
+# )
 
+migrate_data = KubernetesPodOperator(
+        namespace='default',
+        image='kubernetesetlcontainerregistry.azurecr.io/my-dbt-image:1.0',
+        cmds=["dbt", "run"],
+        arguments=[
+            "--project-dir", "./<project_dir>", "--profiles-dir", "./<project_dir>/profiles"
+        ],
+        name="dbt_transformations",
+        task_id="dbt_transformations",
+        get_logs=True
+    )
 
