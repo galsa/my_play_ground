ea485bb0154ae7f9c6524fbcca7a9f8fb25f1558
galsoetendorp <gal.soetendorp@personetics.com>
2023-12-12 14:12:09 +0200

    change the connection_id

--- dags/dag_airbyte_example2.py
+++ dags/dag_airbyte_example2.py
@@ -7,7 +7,7 @@ from airflow.models import Variable
 from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
 from airflow.utils.dates import days_ago
 
-connection_id = '14032a1e1-c8ad-4fbf-a6ef-92d7e9a5931f'  # Variable.get("AIRBYTE_CONNECTION_ID")
+connection_id = '5872a75d-68ef-4009-9b5c-047f118d2185'  # Variable.get("AIRBYTE_CONNECTION_ID")
 airbyte_connection_id = 'airflow-call-to-airbyte-example'
 
 with DAG(
