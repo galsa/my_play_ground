db53331aa89470411f04b3186905c461758aa7b8
galsoetendorp <gal.soetendorp@personetics.com>
2023-12-12 10:46:33 +0200

    added a combination of airbyte call and the dbt call

--- dags/example_airbyte_dbt_airflow_integration2.py
+++ dags/example_airbyte_dbt_airflow_integration2.py
@@ -1,42 +1,53 @@
-# Required python packages
-from airflow.utils.dates import days_ago
-# from airflow.sensors.external_task_sensor import ExternalTaskSensor
-from datetime import datetime
-from datetime import timedelta
-from airflow import DAG
+from pendulum import datetime
+from airflow.decorators import dag, task
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
-from airflow.providers.airbyte.sensors.airbyte import AirbyteJobSensor
-from airflow_dbt.operators.dbt_operator import DbtRunOperator
+import logging
+
+# Kubernetes parameters
+namespace = 'default'
+image = 'kubernetesetlcontainerregistry.azurecr.io/jaffe-shop-duckdb:1.0'
+airbyte_connection_id = '55ac2529-7881-4059-b82b-203a30d408cc'  # Variable.get("AIRBYTE_CONNECTION_ID")
+in_cluster = True
+
+# get the airflow.task logger
+task_logger = logging.getLogger("airflow.task")
 
 
-AIRBYTE_CONNECTION_ID = "4032a1e1-c8ad-4fbf-a6ef-92d7e9a5931f"
-
-# Basic arguments required by the DAG tasks
-default_args = {
-    "owner": "airflow",  # owner of the DAG
-    "depends_on_past": False,  # no dependence on previous attempts
-    "retries": 1,  # limit to 1 retry
-    # "dir": "~/dbt/insight_history",  # location of the relevant dbt models
-    "retry_delay": timedelta(minutes=1),  # retry failed attempt after 1 minute
-    "start_date": days_ago(1),  # initiate the process yesterday
-    "schedule_interval": "*/5 * * * *",  # trigger the dag every 5 minutes
-}
-
-# Define a DAG
-with DAG(
-        dag_id="example_airbyte_dbt_airflow_integration2",
-        default_args=default_args,
-) as dag:
-    # Airbyte data integration task
-    sync_source_destination = AirbyteTriggerSyncOperator(
-        task_id="airbyte_trigger_sync",
-        airbyte_conn_id="airflow-call-to-airbyte-example",
-        connection_id=AIRBYTE_CONNECTION_ID,
+@dag(
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+    schedule_interval="@daily",
+)
+def my_combined_airbyte_dbt_tasks():
+    run_airbyte_job = AirbyteTriggerSyncOperator(
+        task_id="run_airbyte_job",
+        airbyte_conn_id="airbyte_default",
+        connection_id=airbyte_connection_id,
+        asynchronous=False,
+        timeout=3600,
+        wait_seconds=3,
     )
 
-    # dbt data transformation task
-    dbt_run = DbtRunOperator(
-        task_id="dbt_run",
+    run_dbt_transformations = KubernetesPodOperator(
+        namespace=namespace,
+        image=image,
+        cmds=["/bin/bash", "-c"],  # use bash to run multiple commands
+        arguments=["dbt deps && dbt build --profiles-dir ."],
+        name="dbt_transformations",
+        task_id="run_dbt_transformations",
+        get_logs=True,
+        log_events_on_failure=True,
+        in_cluster=in_cluster,
+        is_delete_operator_pod=True,  # don't delete the pod after task completion (only for testing)
     )
-    # Task sequencing indicating dbt_run must run after sync_source_destination
-    sync_source_destination >> dbt_run
+
+    @task
+    def print_success_message():
+        print("All tasks completed successfully")
+
+    run_airbyte_job >> run_dbt_transformations >> print_success_message()
+
+
+combined_tasks_dag = my_combined_airbyte_dbt_tasks()  # needed to instantiate the DAG and make it discoverable by the
+# Airflow scheduler.
